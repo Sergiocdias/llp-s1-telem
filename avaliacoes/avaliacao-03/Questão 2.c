6cb93e0cc1c2383c8cd6cfa3f69bcff85c453a1a
Sergiocdias <111240022+Sergiocdias@users.noreply.github.com>
2022-10-07 03:33:13 -0300

    Add files via upload

---
+++ avaliacoes/avaliacao-03/Questão 2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int funcao(char frase[]) {
+  int i;
+  for(i=33;frase[i]; i--) {
+     printf(" %c", frase[i]);
+  }
+  return 0;
+}
+int main() {
+
+  int p;
+  char s1[] = "meu nome eh Sergio Cavalcante Dias";
+  p = funcao(s1);  
+  return 0;
+}
