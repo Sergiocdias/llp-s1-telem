67955496a220e364c08e563380c7828845470f85
Sergiocdias <111240022+Sergiocdias@users.noreply.github.com>
2022-12-09 21:28:39 -0300

    Add files via upload

---
+++ avaliacoes/avaliacao-06/Avaliacao 06.c
@@ -0,0 +1,41 @@
+// Aluno: Sergio Cavalcante Dias
+// Avaliacao 06
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main(void)
+{   setlocale(LC_ALL,"Portuguese");
+    FILE *arq;
+    char nome[100];
+    int num[3], i;
+
+    arq = fopen("avaliacao 6 Sergio.txt", "w");
+
+    if(arq == NULL)
+    {
+        printf("Houve um erro na abertura do arquivo");
+        return 1;
+    }
+
+    printf("Escreva o seu nome completo:\n ");
+    gets(nome);
+
+    fprintf(arq, "seu nome é:\n%s\n", nome);
+    
+     for(i=0; i<3; i++){
+     	printf("Digite um número(até três vezes):\n");
+     	scanf("%d",&num[i]);
+     		
+	 }
+	 for(i=0; i<3; i++)
+     	fprintf(arq, "O %dº número digitado foi: %d\n", i+1,num[i]);
+     	
+    fclose(arq);
+
+    printf("Dados salvos no arquivo: avaliacao 6 Sergio!");
+
+    getch();
+    return(0);
+}
